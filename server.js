b7d522a82a5e76f4000526d383b09800f39ce889
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-01-24 20:31:17 +0100

    Added passport and session support

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const passport = require("passport");
 const mainRoutes = require("./routes/main");
 const connectDB = require("./config/database");
 const flash = require("express-flash");
+const logger = require("morgan");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 // !!IMPORTANT TO BE FIRST!! Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -12,8 +15,8 @@ require("dotenv").config({ path: "./config/.env" });
 //Connect do database
 connectDB();
 
-// const connectDB = require("./config/database");
-const logger = require("morgan");
+// Passport config
+require("./config/passport")(passport);
 
 //Using EJS for views
 app.set("view engine", "ejs");
@@ -28,11 +31,25 @@ app.use(express.json());
 //logging
 app.use(logger("dev"));
 
-//Setup Routes for which the server is listening
-app.use("/", mainRoutes);
-
 //Use flash messages for errors, info, ect...
 app.use(flash());
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Setup Routes for which the server is listening
+app.use("/", mainRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
