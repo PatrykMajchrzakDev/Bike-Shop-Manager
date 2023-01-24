05c67c9d66252c1479708a7db8fe7f1ffde64af9
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-01-24 20:28:14 +0100

    Added new routes to feed, login, logout pages

--- routes/main.js
+++ routes/main.js
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
 const authController = require("../controllers/auth");
-const postsController = require("../controllers/posts");
+const dashboardController = require("../controllers/dashboard");
+const { ensureAuth } = require("../middleware/passport");
 
 //Main Routes
 router.get("/", homeController.getIndex);
+router.get("/feed", ensureAuth, dashboardController.getDashboard);
+
+//auth
+router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
+router.post("/login", authController.postLogin);
 router.post("/signup", authController.postSignup);
-// router.get("/feed", postsController.getFeed);
 
 module.exports = router;
