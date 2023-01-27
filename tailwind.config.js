cfd71767b2a67f0c6f3653d33fa25b3738de5748
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-01-27 18:24:18 +0100

    Set up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["../public/**/*.{ejs}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "361px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      //Default site colors
+      "primary-color": "###",
+    },
   },
   plugins: [],
-}
+};
