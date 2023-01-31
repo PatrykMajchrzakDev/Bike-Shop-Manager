e62764413603fb060af3b8841fe86bb7387f81e0
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-01-31 19:32:35 +0100

    Added styling to login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,26 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../public/**/*.{ejs}"],
+  content: ["./views/**/*.{html,ejs}"],
   theme: {
     screens: {
       xs: "361px",
       ...defaultTheme.screens,
     },
+    fontFamily: {
+      sans: ['"Segoe UI"', '"Helvetica Neue"', '"Noto Sans"', "sans-serif"],
+    },
     extend: {
-      //Default site colors
-      "primary-color": "###",
+      colors: {
+        //Default site colors
+        mango: "#f5b700",
+        cultured: "#f7f4f3",
+        nickel: "#717568",
+        eerieBlack: "#191919",
+        vividBurgundy: "#a30b37",
+        blue: "#1a73e8",
+        light: "#f1f5f9",
+      },
     },
   },
   plugins: [],
