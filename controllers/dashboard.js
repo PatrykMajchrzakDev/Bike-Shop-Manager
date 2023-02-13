203b74abd081723e97936dbfd4ea66284369db39
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-02-13 18:41:38 +0100

    Added new routes and controllers for add-user page

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -8,4 +8,11 @@ module.exports = {
       console.log(err);
     }
   },
+  addClient: async (req, res) => {
+    try {
+      res.render("add-client.ejs");
+    } catch (err) {
+      console.log(err);
+    }
+  },
 };
--- routes/main.js
+++ routes/main.js
@@ -8,6 +8,7 @@ const { ensureAuth } = require("../middleware/passport");
 //Main Routes
 router.get("/", homeController.getIndex);
 router.get("/dashboard", ensureAuth, dashboardController.getDashboard);
+router.get("/add-client", ensureAuth, dashboardController.addClient);
 
 //auth
 router.get("/logout", authController.logout);
