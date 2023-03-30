c72e3a0950e3ab76aa2b06b0113f62760497ef9e
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-03-30 19:15:47 +0200

    Added getOrders API call

--- controllers/api.js
+++ controllers/api.js
@@ -20,4 +20,16 @@ module.exports = {
       throw error;
     }
   },
+  getOrders: async (req, res) => {
+    try {
+      const orders = await Order.find().sort({ createdAt: "desc" }).lean();
+
+      //Can use $ne (not equal) to get only orders that dont have status "Done"
+      //     const orders = await Order.find({ status: { $ne: 'Done' } }).sort({ createdAt: "desc" }).lean();
+
+      res.json(orders);
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const apiController = require("../controllers/api");
 const { ensureAuth } = require("../middleware/passport");
+const dashboardController = require("../controllers/dashboard");
 
+// CLIENTS ROUTES
 router.get("/clients", apiController.getClients);
 router.get("/clickedOrderInfo/:id", apiController.getClickedOrderInfo);
 
+// ORDERS ROUTES
+
+router.get("/getListOfOrders", ensureAuth, apiController.getOrders);
 module.exports = router;
